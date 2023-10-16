76bf12c94ebd7a6e1938be9bfa5aa10d90734dda
reckyy <srreki98@gmail.com>
2023-10-16 18:41:12 +0900

    ⚡️ feat: Usersのindex,showアクションを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show]
+
+  def index
+    @users = User.order(:id).page(params[:page])
+  end
+
+  def show; end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def book_params
+    params.require(:user).permit(:email, :post, :address, :self_introduction)
+  end
 end
