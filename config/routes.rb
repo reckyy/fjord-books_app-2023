eaab0d778bcac4c28745526a8fde606f72f82ebd
reckyy <srreki98@gmail.com>
2023-10-16 21:41:22 +0900

    ⚡️ feat: パスワードリセット機能の実装
    実装とはいえ、したのはletter_opener_webのインストールと、ActionMailerのデフォルトを設定しただけ。

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'books#index'
   resources :books
   resources :users
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
