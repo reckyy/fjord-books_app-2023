73d9bf0f888866be0855e1f989c1ae1f4f3255ac
reckyy <srreki98@gmail.com>
2023-10-18 09:37:20 +0900

    ⚡️ feat: validationで画像タイプを制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+  validates :avatar, inclusion: { in: %w[images/jpg images/png images/gif], message: 'はJPG, PNG, GIF形式のものを使用してください。' }
 end
