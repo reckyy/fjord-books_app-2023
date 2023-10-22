c96bcf3722ab59ceafd4805662573b13ca5bd671
reckyy <srreki98@gmail.com>
2023-10-22 12:32:07 +0900

    ⚡️ feat: report投稿機能作成

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,26 @@
+# frozen_string_literal: true
+
 class ReportsController < ApplicationController
   def index
     @reports = Report.includes(:user).order(:id).page(params[:page])
+  end
+
+  def new
+    @report = current_user.reports.new
+  end
+
+  def create
+    @report = current_user.reports.new(report_params)
+    if @report.save
+      redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:title, :content)
   end
 end
