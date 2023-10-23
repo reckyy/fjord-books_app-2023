fbce65a93359285c21a130e85f4d63957f545b5d
reckyy <srreki98@gmail.com>
2023-10-23 21:33:19 +0900

    🐛 fix: user情報が更新できない問題修正
    formのフィールドが間違っていた点と、updateをpermitしていなかった点。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[postal_code address self_introduction])
+    keys = %i[postal_code address self_introduction]
+    devise_parameter_sanitizer.permit(:sign_up, keys:)
+    devise_parameter_sanitizer.permit(:account_update, keys:)
   end
 end
