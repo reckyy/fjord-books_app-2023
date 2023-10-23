f4026dc8ab608e2141fffbc62357595e193d2c02
reckyy <srreki98@gmail.com>
2023-10-23 21:09:07 +0900

    ⚡️ feat: userのeditをDeviseの機能を使うように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,16 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update]
 
   def index
     @users = User.order(:id).page(params[:page])
   end
 
-  def show; end
-
-  def edit
-    redirect_to users_path unless current_user.id == @user.id
-  end
-
-  def update
-    if @user.update(user_params)
-      redirect_to user_url(@user), notice: t('controllers.common.notice_update', name: User.model_name.human)
-    else
-      render :edit, status: :unprocessable_entity
-    end
+  def show
+    @user = User.find(params[:id])
   end
 
   private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
 
   def user_params
     params.require(:user).permit(:email, :postal_code, :address, :self_introduction)
