7bf250447192ce74e8cf5eb66ef5c378cd07e58f
reckyy <srreki98@gmail.com>
2023-10-24 23:14:47 +0900

    ⚡️ feat: 画像タイプのvalidate変更
    エラーメッセージを国際化させた。また、yarnファイル関連のファイルを削除したらなぜかアップロードができなくなったので、validationの仕方を変更。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
-  validates :avatar, inclusion: { in: %w[images/jpg images/png images/gif], message: 'はJPG, PNG, GIF形式のものを使用してください。' }
+  validate :avatar_type
+
+  private
+
+  def avatar_type
+    unless avatar.blob.content_type.in?(%w[images/jpg images/png images/gif])
+      avatar.purge
+      errors.add(:avatar, I18n.t('errors.messages.used_unusable_image_type'))
+    end
+  end
 end
