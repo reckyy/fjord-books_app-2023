9a62a7ee0ca624eb39ea0bfa3eea99ac7536d7b7
reckyy <srreki98@gmail.com>
2023-10-26 17:52:33 +0900

    🐛 fix: 不要なルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   root to: 'books#index'
   resources :books
-  resources :users
+  resources :users, only: %i[index show]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
