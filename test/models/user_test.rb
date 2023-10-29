624e4c7fe06b0ce9d5e580c9cca422a86563f9d9
reckyy <srreki98@gmail.com>
2023-10-29 21:31:45 +0900

    💯 test: Userクラスのモデルテスト実装

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = FactoryBot.create(:user)
+  end
+
+  test '名前有のユーザーなら名前が返る' do
+    @user.name = 'Alice'
+    assert_equal @user.name_or_email, 'Alice'
+  end
+
+  test '名前無のユーザーならemailが返る' do
+    assert_equal @user.name_or_email, @user.email
+  end
 end
