b668e9ed04f00a0fc600c85b9ffda337e50aeade
reckyy <srreki98@gmail.com>
2023-10-30 08:54:41 +0900

    💯 test: ログイン後日報を書くシステムテスト実装

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -3,45 +3,28 @@
 require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
+
   setup do
-    @report = reports(:one)
+    @user = FactoryBot.create(:user)
+    @report = FactoryBot.create(:report)
+    @user_has_report = @report.user
   end
 
-  test 'visiting the index' do
-    visit reports_url
-    assert_selector 'h1', text: 'Reports'
+  def login(user)
+    visit new_user_session_path
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_on 'ログイン'
   end
 
-  test 'should create report' do
-    visit reports_url
-    click_on 'New report'
-
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Create Report'
-
-    assert_text 'Report was successfully created'
-    click_on 'Back'
-  end
-
-  test 'should update Report' do
-    visit report_url(@report)
-    click_on 'Edit this report', match: :first
-
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Update Report'
-
-    assert_text 'Report was successfully updated'
-    click_on 'Back'
-  end
-
-  test 'should destroy Report' do
-    visit report_url(@report)
-    click_on 'Destroy this report', match: :first
-
-    assert_text 'Report was successfully destroyed'
+  test 'ログイン後、日報を書く' do
+    login(@user)
+    click_on '日報'
+    click_on '日報の新規作成'
+    fill_in 'report[title]', with: 'テストタイトル'
+    fill_in 'report[content]', with: 'テスト内容'
+    click_on '登録する'
+    assert_text '日報が作成されました。'
   end
 end
