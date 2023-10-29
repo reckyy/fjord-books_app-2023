4eaaab7fb3e537f49c8e2c43c27769a18754f00b
reckyy <srreki98@gmail.com>
2023-10-29 22:18:26 +0900

    💯 test: Report#editable?のテスト実装

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,19 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = FactoryBot.create(:user)
+    @report = FactoryBot.create(:report)
+    @user_has_report = @report.user
+  end
+
+  test '日報作成者なら編集できる' do
+    assert @report.editable?(@user_has_report)
+  end
+
+  test '日報作成者以外なら編集できない' do
+    assert_not @report.editable?(@user)
+  end
 end
