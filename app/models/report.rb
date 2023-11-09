d4017d172341b1a3d5869dfc6fb1c4357e9202d1
reckyy <srreki98@gmail.com>
2023-11-09 11:10:48 +0900

    🐛 fix: モデル名の変更に伴い、has_many関連も修正

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,10 +3,10 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
-  has_many :mention_reports, dependent: :destroy
-  has_many :mentioning_reports, through: :mention_reports, source: :mentioned_report
-  has_many :swapping_id_mention_reports, class_name: 'MentionReport', foreign_key: 'mentioned_report_id', dependent: :destroy, inverse_of: 'mentioned_report'
-  has_many :mentioned_reports, through: :swapping_id_mention_reports, source: :report
+  has_many :report_mentions, dependent: :destroy
+  has_many :mentioning_reports, through: :report_mentions, source: :mentioned_report
+  has_many :inverse_of_report_mentions, class_name: 'ReportMention', foreign_key: 'mentioned_report_id', dependent: :destroy, inverse_of: 'mentioned_report'
+  has_many :mentioned_reports, through: :inverse_of_report_mentions, source: :report
 
   validates :title, presence: true
   validates :content, presence: true
