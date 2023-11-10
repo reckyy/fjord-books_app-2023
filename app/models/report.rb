c468171b22f5a2f576135887840943b67910e73c
reckyy <srreki98@gmail.com>
2023-11-10 22:09:29 +0900

    🐛 fix: 参照先を記述し忘れ

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,10 +3,10 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
-  has_many :report_mentions, dependent: :destroy
-  has_many :mentioning_reports, through: :report_mentions, source: :mentioned_report
-  has_many :inverse_of_report_mentions, class_name: 'ReportMention', foreign_key: 'mentioned_report_id', dependent: :destroy, inverse_of: 'mentioned_report'
-  has_many :mentioned_reports, through: :inverse_of_report_mentions, source: :report
+  has_many :report_mentioner, class_name: 'ReportMention', dependent: :destroy
+  has_many :mentioning_reports, through: :report_mentioner, source: :mentioned_report
+  has_many :report_mentionee, class_name: 'ReportMention', foreign_key: 'mentioned_report_id', dependent: :destroy, inverse_of: 'mentioned_report'
+  has_many :mentioned_reports, through: :report_mentionee, source: :report
 
   validates :title, presence: true
   validates :content, presence: true
@@ -34,7 +34,7 @@ class Report < ApplicationRecord
   end
 
   def save_mentions(report_id)
-    report_mentions.destroy_all
+    report_mentioner.destroy_all
     mentioned_ids = content.to_s.scan(REPORT_URL).flatten.uniq
     return true if mentioned_ids.empty?
 
